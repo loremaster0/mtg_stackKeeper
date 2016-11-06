45fe9835a61403bb905ae9cbb6628446b968e18d
Christopher Allen Haines <cahaines@ymail.com>
2016-11-05 21:13:44 -0500

    main coding finished, readme updated, app is ready for use

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # mtg_stackKeeper
 A "Stack" app. for 'Magic: The Gathering TCG'
 
+=====
+
 To start the app.: Open the 'index.html' file in a web browser.
 
+=====
+
+Links to the works of others that I used in this app.:
+
+"Reset CSS" [public domain] --> http://meyerweb.com/eric/tools/css/reset/
+
+"Jquery (ver. 3.1.1)" [MIT license] --> https://jquery.com/download/
+
+"FredokaOne-Regular.ttf (Font File)" [Open Font License ] --> https://fonts.google.com/specimen/Fredoka+One
--- mtgsk_script.js
+++ mtgsk_script.js
@@ -2,7 +2,7 @@
 var theStack = []; //master array
 
 // add-to-stack function
-var addToStack = function () {
+var addToStack = function() {
 	//read input form
 	var inputName, inputControl, inputNotes;
 	inputName = $("#itemName").val();
@@ -20,34 +20,38 @@ var addToStack = function () {
 				$("#itemName").val("");
 				$("#itemControl").val("");
 				$("#itemNotes").val("");
-				return true;
+				displayStack();
 			} else {
 				//EEROR: no notes
 				console.log("ERROR - No Notes");
 				alert("No Notes!");
 				$("#itemNotes").focus();
-				return false;
 			}
 		} else {
 			//ERROR: no controller
 			console.log("ERROR - No Controller");
 			alert("No Controller!");
 			$("#itemControl").focus();
-			return false;
 		}
 	} else {
 		//ERROR: no name
 		console.log("ERROR - No Name");
-		alert("No Nmae!");
+		alert("No Name!");
 		$("#itemName").focus();
-		return false;
 	}
-}
+};
 
 // resolve-top-layer-stack function
+var resolveTopStack = function() {
+	console.log("Resolved Stack Item!");
+	console.log(theStack);
+	theStack.shift();
+	console.log(theStack);
+	displayStack();
+};
 
 // full-clear-stack function
-var fullClear = function () {
+var fullClear = function() {
 	console.log("Full clear stack!");
 	console.log(theStack);
 	theStack.splice(0, theStack.length);
@@ -55,13 +59,45 @@ var fullClear = function () {
 	$("#itemName").val("");
 	$("#itemControl").val("");
 	$("#itemNotes").val("");
-}
+	displayStack();
+};
 
 // stack-display function
+var displayStack = function() {
+	if (theStack.length == 0) {
+		// stack is clear
+		$("#theStackList").html("-- Stack Clear --");
+	} else {
+		// stack has stuff in it
+		var stackCode = ""; // variable holds the html code for the stack
+		stackCode += "<p>\n";
+		stackCode += "Spell\/Ability: " + theStack[0][0] + "<br>\n";
+		stackCode += "Owner\/Controller: " + theStack[0][1] + "<br>\n";
+		stackCode += "Note(s): " + theStack[0][2] + "<br>\n";
+		stackCode += "<button id=\"resolveItem\">Stack Item Resolved<\/button>\n";
+		stackCode += "<\/p>\n\n";
+		if (theStack.length > 1) {
+			for (var i = 1; i < theStack.length; i++) {
+				stackCode += "<p>\n";
+				stackCode += "Spell\/Ability: " + theStack[i][0] + "<br>\n";
+				stackCode += "Owner\/Controller: " + theStack[i][1] + "<br>\n";
+				stackCode += "Note(s): " + theStack[i][2] + "<br>\n";
+				stackCode += "<\/p>\n\n";	
+			}
+		}
+		$("#theStackList").html(stackCode);
+		$("#resolveItem").click(resolveTopStack);
+		$("#itemName").focus();
+	}
+};
 
 // jquery-doc-ready function
 $(document).ready(function() {
+	console.log("Ready!");
 	$("#itemName").focus();
 	$("#stackAdd").click(addToStack);
 	$("#stackClear").click(fullClear);
+	$("#aboutApp").click(function() {
+		alert("\"MTG - Stack Keeper\" coded by: Loremaster0");
+	});
 });
